a1255edba5a5e69312a25e4ef70a7ae00aa44d3e
Anastasiya Demidchik <iskhakova.anastasiya@gmail.com>
2023-01-16 12:08:31 +0100

    feat: add code js for task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
+const { neq } = require("semver");
+
 module.exports = function reverse (n) {
-  let a = Math.abs(n);
-  let array = [...a.toString()];
- 
-  return array.reverse().join("");
+let num = Math.abs(n)  
+let arr = String(num).split('');
+let arrNew = [];
+
+for (let i = 0; i < arr.length; i++) {
+    arrNew.push(arr.pop())
+}
+
+arrNew.push(arr.shift());
+
+return Number(arrNew.join(''));
 }
